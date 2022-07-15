e9629fc09e93532f95e9d9b562b68355ece8fd93
Malte Dostal <malte.dostal@gmail.com>
2022-07-15 21:19:20 +0200

    refactor var keyword to let

--- src/keyword.rs
+++ src/keyword.rs
@@ -1,16 +1,16 @@
 use std::collections::HashMap;
 
-pub const KW_VAR: &str = "var";
+pub const KW_LET: &str = "let";
 pub const KW_FUN: &str = "fun";
 
 pub enum Keyword {
-    Var,
+    Let,
     Fun,
 }
 
 pub fn keywords() -> HashMap<String, Keyword> {
     let mut kws: HashMap<String, Keyword> = HashMap::with_capacity(2);
-    kws.insert(KW_VAR.into(), Keyword::Var);
+    kws.insert(KW_LET.into(), Keyword::Let);
     kws.insert(KW_FUN.into(), Keyword::Fun);
     kws
 }
